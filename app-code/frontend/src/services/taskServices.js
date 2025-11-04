794395e8780a4ddaf119db749180f899b1f6b8c1
madelsattar413 <mabdelsattar413@gmail.com>
2025-11-04 22:43:30 +0200

    fixed back-db communication

--- app-code/frontend/src/services/taskServices.js
+++ app-code/frontend/src/services/taskServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //const apiUrl = "http://k8s-default-backserv-a10bbd9324-c9a0679ddf46d492.elb.us-east-1.amazonaws.com:8080/api/tasks";
-const apiUrl = "http://back-service:8080/api/tasks"
+const apiUrl = "/api/tasks"
 console.log(apiUrl)
 export function getTasks() {
     return axios.get(apiUrl);
