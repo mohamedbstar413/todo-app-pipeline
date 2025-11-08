30709e0029e0875188be4528c753987d1be0efaf
madelsattar413 <mabdelsattar413@gmail.com>
2025-11-08 19:03:53 +0200

    reformating directories

--- infra/kubernetes-resources/data.tf
+++ infra/kubernetes-resources/data.tf
@@ -19,12 +19,3 @@ data "aws_eks_cluster" "todo_cluster" {
   depends_on = [ var.todo_cluster ]
   name = "todo-app-cluster"
 }
-/*data "aws_iam_openid_connect_provider" "oidc" {
-  url = data.aws_eks_cluster.todo_cluster.identity[0].oidc[0].issuer
-}*/
-
-/*resource "aws_iam_openid_connect_provider" "oidc" {
-  url = data.aws_eks_cluster.todo_cluster.identity[0].oidc[0].issuer
-  client_id_list = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks_oidc_cert.certificates[0].sha1_fingerprint]
-}*/
